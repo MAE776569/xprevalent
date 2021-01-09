086689b4b8c118d13fdc453ff52f5db2d5bd5d02
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-01-09 16:21:49 +0200

    Make base controller non-abstract

--- src/base/BaseController.ts
+++ src/base/BaseController.ts
@@ -1,19 +1,51 @@
 import { Request, Response, NextFunction } from "express";
 import { Model, Document } from "mongoose";
+import { FillableObject } from "../types/base";
 
-export abstract class BaseController {
+class BaseController {
   // Request Handler fields
-  protected abstract readonly req: Request;
-  protected abstract readonly res: Response;
-  protected abstract readonly next: NextFunction;
+  protected readonly req: Request;
+  protected readonly res: Response;
+  protected readonly next: NextFunction;
 
-  // Model and query Fields
-  protected abstract queryObjectName: string;
-  protected abstract readonly model: Model<Document>;
+  // Model and query result object name
+  protected readonly model?: Model<Document>;
+  protected queryObjectName?: string = "data";
 
-  protected abstract getContextObject(): Promise<FillableObject>;
+  // Query fields
+  protected queryFilter: FillableObject = {};
+  protected selectedFields?: string[];
+  protected excludedFields?: string[];
+  protected sortBy?: FillableObject;
+  protected populatedFields?: string[];
 
-  protected abstract getQueryResult(): Promise<Document[] | Document | null>;
+  constructor(req: Request, res: Response, next: NextFunction) {
+    this.req = req;
+    this.res = res;
+    this.next = next;
+  }
 
-  protected abstract handleRequest(): Promise<Response | void>;
+  protected getContextObject(): Promise<FillableObject> {
+    return Promise.resolve({});
+  }
+
+  protected getQueryResult(): Promise<Document[] | Document | null> {
+    return Promise.resolve([]);
+  }
+
+  protected async handleRequest(): Promise<Response | void> {
+    try {
+      const contextObject = await this.getContextObject();
+      const resObject = { ...contextObject };
+      if (this.model) {
+        const queryResult = await this.getQueryResult();
+        resObject[this.queryObjectName!] = queryResult;
+      }
+      return this.res.send(contextObject);
+    } catch (err) {
+      return this.next(err);
+    }
+  }
 }
+
+export = BaseController;

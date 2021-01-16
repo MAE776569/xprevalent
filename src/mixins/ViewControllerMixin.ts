5eb0435edbc533d2c0b68edb40e78374ff917b9f
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-01-16 17:16:18 +0200

    Created view controller mixin

---
+++ src/mixins/ViewControllerMixin.ts
@@ -0,0 +1,27 @@
+import BaseController from "../base/BaseController";
+import { Constructor } from "../types/base";
+
+function ViewControllerMixin<T extends Constructor<BaseController>>(
+  BaseClass: T
+): T {
+  return class ViewController extends BaseClass {
+    // View template
+    protected viewTemplate!: string;
+
+    protected async handleRequest() {
+      try {
+        const contextObject = await this.getContextObject();
+        const locals = { ...contextObject };
+        if (this.model) {
+          const queryResult = await this.getQueryResult();
+          locals[this.queryObjectName!] = queryResult;
+        }
+        return this.res.render(this.viewTemplate, locals);
+      } catch (err) {
+        return this.next(err);
+      }
+    }
+  };
+}
+
+export = ViewControllerMixin;

fe7b3cc9d511d31b0c7792cc0a15ee5215e50164
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-01-26 15:59:30 +0200

    style: format base types

--- src/types/base.d.ts
+++ src/types/base.d.ts
@@ -1,10 +1,8 @@
 export interface FillableObject {
   [key: string]: any;
 }
-
 // eslint-disable-next-line no-unused-vars
 export type Constructor<T> = new (...args: any[]) => T;
-
 export interface SortObject {
   [key: string]: 1 | -1;
 }

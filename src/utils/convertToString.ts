9fb9bb2cba4b6cf51d7c5b702155e69293841083
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-02-02 16:28:59 +0200

    feat: created convert to string helper

---
+++ src/utils/convertToString.ts
@@ -0,0 +1,20 @@
+function convertToString(value: any, deep = true): string {
+  if (Array.isArray(value) && value.length && deep) {
+    return convertToString(value[0], false);
+  } else if (value instanceof Date) {
+    return value.toISOString();
+  } else if (value && typeof value === "object" && value !== null) {
+    return JSON.stringify(value);
+  } else if (value && typeof value === "object" && value.toString) {
+    if (typeof value.toString !== "function") {
+      return Object.getPrototypeOf(value).toString.call(value);
+    }
+    return value.toString();
+  } else if (value == null || (isNaN(value) && !value.length)) {
+    return "";
+  }
+
+  return String(value);
+}
+
+export = convertToString;

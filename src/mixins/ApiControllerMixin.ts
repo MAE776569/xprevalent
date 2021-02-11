1c6603f7e5a25e82ddfc5ec867893626bedec4a6
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-02-11 15:49:20 +0200

    Make query object implicit in api mixin

--- src/mixins/ApiControllerMixin.ts
+++ src/mixins/ApiControllerMixin.ts
@@ -5,16 +5,13 @@ function ApiControllerMixin<T extends Constructor<BaseController>>(
   BaseClass: T
 ): T {
   return class ApiController extends BaseClass {
-    // Query result object name
-    protected queryObjectName!: string;
-
     protected async handleRequest() {
       try {
         const contextObject = await this.getContextObject();
         const resObject = { ...contextObject };
         if (this.model) {
           const queryResult = await this.getQueryResult();
-          resObject[this.queryObjectName] = queryResult;
+          resObject[this.queryObjectName!] = queryResult;
         }
         return this.res.json(resObject);
       } catch (err) {

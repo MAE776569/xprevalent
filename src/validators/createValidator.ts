7f3e75b6507bf176e3ae5064215210b027d2d986
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-03-07 14:21:04 +0200

    feat: add create validators function factory

---
+++ src/validators/createValidator.ts
@@ -0,0 +1,25 @@
+import { ValidationLocation } from "../types/validator";
+import bindObject from "../utils/bindObject";
+import ValidationChain from "./ValidationChain";
+import ValidationPipeline from "./ValidationPipeline";
+import { Chain, ValidationInput } from "../types/validation/schema";
+
+function createValidator(
+  location: ValidationLocation,
+  message?: string
+): Chain {
+  const pipeline = new ValidationPipeline(location);
+  if (message) pipeline.setMessage(message);
+  const pipelineRunner = function ({
+    req,
+    keys,
+    validationResult
+  }: ValidationInput) {
+    pipeline.setRequst(req).setKeys(keys).setValidationResult(validationResult);
+    pipeline.run();
+  };
+  const validator = new ValidationChain(pipeline, pipelineRunner);
+  return Object.assign(pipelineRunner, bindObject(validator));
+}
+
+export = createValidator;

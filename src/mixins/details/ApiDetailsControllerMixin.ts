eaee1d988f0d64d653de5593a985606ce6dcd1f8
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-03-31 14:47:14 +0200

    feat: created api details controller mixin

---
+++ src/mixins/details/ApiDetailsControllerMixin.ts
@@ -0,0 +1,29 @@
+import BaseDetailsController from "../../base/BaseDetailsController";
+import { Constructor } from "../../types/base";
+
+function ApiDetailsControllerMixin<
+  T extends Constructor<BaseDetailsController>
+>(BaseClass: T): T {
+  return class ApiDetailsController extends BaseClass {
+    protected async handleRequest() {
+      try {
+        const validationResult = this.validationSchema.validate(this.req);
+        if (validationResult.hasError(this.keyParam) && !this.findOne)
+          return this.res.status(404).json({ message: "Invalid id" });
+
+        const queryResult = await this.getQueryResult();
+        if (!queryResult)
+          return this.res.status(404).json({ message: "Not found" });
+
+        const contextObject = await this.getContextObject();
+        const resObject = { ...contextObject };
+        resObject[this.queryObjectName] = queryResult;
+        return this.res.json(resObject);
+      } catch (err) {
+        return this.next(err);
+      }
+    }
+  };
+}
+
+export = ApiDetailsControllerMixin;

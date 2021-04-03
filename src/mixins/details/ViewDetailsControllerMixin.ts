152d1964795a43c0fc4c0f2698aa65c2599bd332
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-04-03 13:28:39 +0200

    feat: created view details controller mixin

---
+++ src/mixins/details/ViewDetailsControllerMixin.ts
@@ -0,0 +1,31 @@
+import BaseDetailsController from "../../base/BaseDetailsController";
+import { Constructor } from "../../types/base";
+
+function ViewDetailsControllerMixin<
+  T extends Constructor<BaseDetailsController>
+>(BaseClass: T): T {
+  return class ViewController extends BaseClass {
+    // View template
+    protected viewTemplate!: string;
+
+    protected async handleRequest() {
+      try {
+        const validationResult = this.validationSchema.validate(this.req);
+        if (validationResult.hasError(this.keyParam) && !this.findOne)
+          return this.res.sendStatus(404);
+
+        const queryResult = await this.getQueryResult();
+        if (!queryResult) return this.res.sendStatus(404);
+
+        const contextObject = await this.getContextObject();
+        const locals = { ...contextObject };
+        locals[this.queryObjectName] = queryResult;
+        return this.res.render(this.viewTemplate, locals);
+      } catch (err) {
+        return this.next(err);
+      }
+    }
+  };
+}
+
+export = ViewDetailsControllerMixin;

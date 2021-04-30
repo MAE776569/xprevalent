1453a41d094c81c38b02beb8f29798afb73f30f5
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-04-30 14:56:34 +0200

    Created api create mixin

---
+++ src/mixins/create/ApiCreateControllerMixin.ts
@@ -0,0 +1,27 @@
+import BaseCreateController from "../../base/form/BaseCreateController";
+import { Constructor } from "../../types/controllers/generic";
+
+function ApiCreateControllerMixin<T extends Constructor<BaseCreateController>>(
+  BaseClass: T
+): T {
+  return class ApiCreateController extends BaseClass {
+    protected async handleRequest() {
+      try {
+        if (this.validationResult.hasError())
+          return this.res
+            .status(422)
+            .json({ errors: this.validationResult.getErrors() });
+
+        const queryResult = await this.getQueryResult();
+        const contextObject = await this.getContextObject();
+        const resObject = { ...contextObject };
+        resObject[this.queryObjectName] = queryResult;
+        return this.res.status(201).json(resObject);
+      } catch (err) {
+        return this.next(err);
+      }
+    }
+  };
+}
+
+export = ApiCreateControllerMixin;

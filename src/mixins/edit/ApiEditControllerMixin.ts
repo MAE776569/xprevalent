11907d57bdc1098b42d4a15031d6f01dace66762
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-05-23 13:58:14 +0200

    feat: created edit api controller mixin

---
+++ src/mixins/edit/ApiEditControllerMixin.ts
@@ -0,0 +1,34 @@
+import BaseEditController from "../../base/form/BaseEditController";
+import { Constructor } from "../../types/controllers/generic";
+
+function ApiEditControllerMixin<T extends Constructor<BaseEditController>>(
+  BaseClass: T
+): T {
+  return class ApiEditController extends BaseClass {
+    protected async handleRequest() {
+      try {
+        const paramValidationResult = this.paramValidation.validate(this.req);
+        if (paramValidationResult.hasError(this.keyParam) && !this.updateOne)
+          return this.res.status(404).json({ message: "Invalid id" });
+
+        if (this.validationResult.hasError())
+          return this.res
+            .status(422)
+            .json({ errors: this.validationResult.getErrors() });
+
+        const queryResult = await this.getQueryResult();
+        if (!queryResult)
+          return this.res.status(404).json({ message: "Not found" });
+
+        const contextObject = await this.getContextObject();
+        const resObject = { ...contextObject };
+        resObject[this.queryObjectName] = queryResult;
+        return this.res.json(resObject);
+      } catch (err) {
+        return this.next(err);
+      }
+    }
+  };
+}
+
+export = ApiEditControllerMixin;

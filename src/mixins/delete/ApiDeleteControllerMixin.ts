24e8fe345d38a012b01a73dce6d4ddc25671738d
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-05-27 14:37:24 +0200

    Created api delete controller mixin

---
+++ src/mixins/delete/ApiDeleteControllerMixin.ts
@@ -0,0 +1,29 @@
+import BaseDeleteController from "../../base/form/BaseDeleteController";
+import { Constructor } from "../../types/controllers/generic";
+
+function ApiDeleteControllerMixin<T extends Constructor<BaseDeleteController>>(
+  BaseClass: T
+): T {
+  return class ApiDeleteController extends BaseClass {
+    protected async handleRequest() {
+      try {
+        const validationResult = this.validationSchema.validate(this.req);
+        if (validationResult.hasError(this.keyParam) && !this.deleteOne)
+          return this.res.status(404).json({ message: "Invalid id" });
+
+        const queryResult = await this.getQueryResult();
+        if (!queryResult)
+          return this.res.status(404).json({ message: "Not found" });
+
+        const contextObject = await this.getContextObject();
+        const resObject = { ...contextObject };
+        resObject[this.queryObjectName] = queryResult;
+        return this.res.json(resObject);
+      } catch (err) {
+        return this.next(err);
+      }
+    }
+  };
+}
+
+export = ApiDeleteControllerMixin;

bc00097b7048f2cb40d601e882889e116e147e76
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-05-28 14:59:42 +0200

    Add delete type to view form types

--- src/types/controllers/form.d.ts
+++ src/types/controllers/form.d.ts
@@ -1,9 +1,11 @@
 import BaseCreateController from "../../base/form/BaseCreateController";
+import BaseDeleteController from "../../base/form/BaseDeleteController";
 import BaseEditController from "../../base/form/BaseEditController";
 import FormControllerMixin from "../../mixins/form/FormControllerMixin";
 
 export type ViewFormControllerType = (
   | BaseCreateController
   | BaseEditController
+  | BaseDeleteController
 ) &
   ReturnType<typeof FormControllerMixin>;

5ad7ae5d9a99f4dd83dff486e800824fc86be92e
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2022-01-22 18:19:37 +0200

    feat: add new schema validation types

--- src/types/validation/schema.d.ts
+++ src/types/validation/schema.d.ts
@@ -1,17 +1,26 @@
 /* eslint-disable no-unused-vars */
 import { FillableObject } from "../controllers/generic";
 import { AnyObjectSchema } from "yup";
+import ValidationResult from "../../validators/ValidationResult";
 
 export interface ValidationErrors extends FillableObject, Object {}
 
-interface SchemaValidation {
+export interface SchemaValidation {
   body?: AnyObjectSchema;
   params?: AnyObjectSchema;
   query?: AnyObjectSchema;
 }
 
 export interface ValidationObject {
-  hasError: (name?: string) => boolean;
+  hasError: (validationResultInput?: ValidationResultInput) => boolean;
   getSanitizedValue: (name?: string) => any;
-  getErrors: () => ValidationErrors;
+  getErrors: (
+    validationResultInput?: ValidationResultInput
+  ) => ValidationErrors;
+}
+
+export type ValidationLocation = "body" | "params" | "query";
+export interface ValidationResultInput {
+  name?: string;
+  location?: ValidationLocation;
 }

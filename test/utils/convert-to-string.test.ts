ca5e32a545af8470ffb5ffefc360e6d772a47147
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2022-03-04 18:01:39 +0200

    test: add test cases for convert to string util

---
+++ test/utils/convert-to-string.test.ts
@@ -0,0 +1,38 @@
+import convertToString from "../../src/utils/convertToString";
+
+describe("Should test converting objects to string", () => {
+  it("Should convert array to string", () => {
+    expect(convertToString([1])).toBe("1");
+  });
+
+  it("Should convert date to string", () => {
+    const date = new Date();
+    expect(convertToString(date)).toBe(date.toISOString());
+  });
+
+  it("Should convert object to string", () => {
+    const object = { convertToString: true };
+    expect(convertToString(object)).toBe(JSON.stringify(object));
+  });
+
+  it("Should convert null to string", () => {
+    expect(convertToString(null)).toBe("");
+  });
+
+  it("Should convert NaN to string", () => {
+    expect(convertToString(NaN)).toBe("");
+  });
+
+  it("Should convert functions to strings", () => {
+    const genericFunction = () => {};
+    expect(convertToString(genericFunction)).toBe(genericFunction.toString());
+  });
+
+  it("Should convert objects with generic toString to string", () => {
+    function genericFunction() {}
+    genericFunction.toString = "Functions are objects";
+    expect(convertToString(genericFunction)).toBe(
+      Object.getPrototypeOf(genericFunction).toString.call(genericFunction)
+    );
+  });
+});

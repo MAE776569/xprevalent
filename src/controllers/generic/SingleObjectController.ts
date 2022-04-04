fe98828fd2119bd433176a9b8484f169948f1f24
Mohamed Ahmed <35629390+MAE776569@users.noreply.github.com>
2022-04-04 14:20:37 +0200

    fix: run validation with each request (#11)

--- src/controllers/generic/SingleObjectController.ts
+++ src/controllers/generic/SingleObjectController.ts
@@ -6,13 +6,9 @@ class SingleObjectController extends BaseController {
   // Params and validation
   protected idParam: string = "id";
   protected validationSchema!: ValidationSchema;
-  private validation!: ValidationObject;
 
   protected get validationResult(): ValidationObject {
-    if (!this.validation) {
-      this.validation = this.validationSchema.validate(this.req);
-    }
-    return this.validation;
+    return this.validationSchema.validate(this.req);
   }
 
   protected validateIdParam(): boolean {
